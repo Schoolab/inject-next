af8e1319075fbcba3c6f39c5b2a1612dcadaae9d
terenceIrv <terence@theschoolab.com>
2024-10-03 15:02:37 +0400

    update project overview

--- src/app/pages/project/Studies/ProjectOverview.tsx
+++ src/app/pages/project/Studies/ProjectOverview.tsx
@@ -540,7 +540,7 @@ export const ProjectOverview = () => {
                     </div>
                     <div className="col-12 col-lg-6 col-xl-4">
                         <div className="application-section">
-                            <h3 className="application-section--title">Project progression</h3>
+                            <h3 className="application-section--title">Progression</h3>
                             <div className="row">
                                 <div className=" col-lg-12 col-md-6 col-12 mb-5 d-flex align-items-stretch">
                                     <div className="bg-light rounded m-0 p-7 flex-fill d-flex flex-row">
