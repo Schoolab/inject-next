58270f3297f3830c3d7df2744aea3445f1ad23e4
SachaJolly <alex.sacha.jolly@gmail.com>
2024-12-12 19:21:49 +0100

    update card application

--- src/stories/components/Cards/CardApplication.stories.ts
+++ src/stories/components/Cards/CardApplication.stories.ts
@@ -3,14 +3,20 @@ import { CardApplication } from "@/app/components/cards/CardApplication";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-    title: "Components/Cards/Participant/Application",
+    title: "Components/Cards/Application",
     component: CardApplication,
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-        layout: "padded",
+        layout: "centered",
         docs: {
             controls: { sort: "requiredFirst" },
         },
+    },
+     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+     tags: ["autodocs"],
+     argTypes: {
+    },
+    args: {
     },
 
 } satisfies Meta<typeof CardApplication>;
@@ -20,6 +26,6 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 
-export const Application: Story = {
-
+export const Default: Story = {
+    args: {},
 };
