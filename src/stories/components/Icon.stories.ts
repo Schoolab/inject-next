5d7f62da03274745afe9c2885c8c29c6948695f7
SachaJolly <alex.sacha.jolly@gmail.com>
2025-01-17 17:42:22 +0100

    update icons list

--- src/stories/components/Icon.stories.ts
+++ src/stories/components/Icon.stories.ts
@@ -1,24 +1,28 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { IconsList } from '@/app/pages/icons/IconsList';
-
+import { Icon } from '@/app/components/Icon';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Components/Icons/List',
-  component: IconsList,
+  title: 'Components/Icon',
+  component: Icon,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
+    layout: 'padded',
   },
+       // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+       tags: ["autodocs"],
 
-} satisfies Meta<typeof IconsList>;
+} satisfies Meta<typeof Icon>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const List: Story = {
-
+export const Default: Story = {
+  args: {
+    name: "account",
+    size: "md",    
+  }
 };
 
 
